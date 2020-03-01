23c14f9d4ee1783b81954d1591521b57cece1851
msk-1 <h69.masaki@gmail.com>
2020-03-01 23:48:10 +0900

    start three js viewer

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './app.css';
+import CodeViewer from './CodeViewer'
+
+const App = () => (
+  <div class="ui center aligned padded grid">
+    <CodeViewer />
+  </div>
+)
+
+export default App
---
+++ src/components/CodeViewer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './codeViewer.css';
+
+const CodeViewer = () => (
+    <div className='eight wide column'>
+        <h1>Code</h1>
+        <div class="ui text container">
+            asdas
+        </div>
+    </div>
+)
+
+export default CodeViewer
